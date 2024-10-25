e4267ea97011db98541aeca5fa8899c8444f6a0e
Roza. <71328419+shinyPy@users.noreply.github.com>
2024-10-25 22:51:10 +0800

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.whenReady().then(() => {
   autoUpdater.on('update-available', () => {
     dialog.showMessageBox({
       type: 'info',
-      title: 'Update Available. S',
+      title: 'Update Available.',
       message: 'A new version is available. It will be downloaded in the background.',
     });
   });
